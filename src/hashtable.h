4019e62098e36a055422012b1c1baa0e9984c599
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-11-06 12:19:24 -0400

    -

--- src/hashtable.h
+++ src/hashtable.h
@@ -2,107 +2,109 @@
 #ifndef EASYFS_HASHTABLE_HEADER
 #define EASYFS_HASHTABLE_HEADER
 
+#include <functional>
+#include <sstream>
 #include <tuple>
 #include <vector>
-#include <sstream>
-#include <functional>
+
+#include <iostream>
 
 #include "siphash.h"
 
 namespace easyfs {
 
-const char* SALT = "dW8(2!?GTfDFJ@Le";
+const char *SALT = "dW8(2!?GTfDFJ@Le";
 
-template<typename T>
-struct Hash {
-    static uint64_t hash (T const& k) noexcept{
+template <typename T> struct Hash {
+    static uint64_t hash(T const &k) noexcept {
         uint64_t value = 0;
-        siphash(
-            (const void*)&k, 
-            (size_t) sizeof(T), 
-            SALT, 
-            (uint8_t*)&value, 
-            (size_t) sizeof(uint64_t)
-        );
+        siphash((const void *)&k, (size_t)sizeof(T), SALT, (uint8_t *)&value,
+                (size_t)sizeof(uint64_t));
         return value;
     }
 };
 
-template<>
-struct Hash<std::string> {
-    static uint64_t hash (std::string const& k) noexcept{
+template <> struct Hash<std::string> {
+    static uint64_t hash(std::string const &k) noexcept {
         uint64_t value = 0;
-        siphash(
-            (const void*)&k[0], 
-            (size_t) k.size(), 
-            SALT, 
-            (uint8_t*)&value, 
-            (size_t) sizeof(uint64_t)
-        );
+        siphash((const void *)&k[0], (size_t)k.size(), SALT, (uint8_t *)&value,
+                (size_t)sizeof(uint64_t));
 
         return value;
     }
 };
-
 
 #define FAST_MOD(i, mod) ((i < mod) * i + (i - mod) * (i > mod))
 #define REG_MOD(i, mod) i % mod
+#define P2_MOD(i, mod) (i & (mod - 1))
 
-inline int fast_mod(int i, int mod) {
-    return FAST_MOD(i, mod);
-}
+inline uint64_t fast_mod(uint64_t i, uint64_t mod) { return FAST_MOD(i, mod); }
 
-inline int reg_mod(int i, int mod) {
-    return REG_MOD(i, mod);
-}
+inline uint64_t reg_mod(uint64_t i, uint64_t mod) { return REG_MOD(i, mod); }
+
+// mod must be a power of 2
+inline uint64_t fast_mod_p2(uint64_t i, uint64_t mod) { return P2_MOD(i, mod); }
+
+inline bool is_power2(uint64_t x) { return (x != 0) && ((x & (x - 1)) == 0); }
 
 // TODO: rewrite this with a Storage for Key-Values
-// and a HashTable for Key->Index 
+// and a HashTable for Key->Index
 // the HasTable will be bigger than the number of values so it is useful
 // to make it as small as possible
 // but then it is not exactly linear probing
-template<typename Key, typename Value, typename H = Hash<Key>>
-struct HashTable {
-private:
-    struct Item {
-        Item():
-            key(Key())
-        {}
 
-        Item(Key const& k, Value const& v):
-            key(k), value(v), used(true)
-        {}
+/* Hashtable with Open addressing and linear probing.
+ *
+ * Linear probing improves lookup times due to locality of references
+ *
+ * The table is rehashed once we reach a load factor of 0.75,
+ * rehashing less or more frequently would impact performance negatively.
+ *
+ * Everytime the table needs to be rehashed the storage size is doubled
+ *
+ */
+template <typename Key, typename Value, typename H = Hash<Key>> struct HashTable {
+    private:
+    struct Item {
+        Item() : key(Key()) {}
+
+        Item(Key const &k, Value const &v) : key(k), value(v), used(true) {}
 
         Key const key;
-        Value value;
-        bool used: 1 = false;
-        bool deleted: 1 = true;
+        Value     value;
+        bool      used : 1    = false;
+        bool      deleted : 1 = true;
 
-        Item& operator= (Item const& i) {
-            Key& mutkey = (Key&)key;
-            mutkey = i.key;
-            value = i.value;
-            used = true;
-            deleted = false;
+        Item &operator=(Item const &i) {
+            Key &mutkey = (Key &)key;
+            mutkey      = i.key;
+            value       = i.value;
+            used        = true;
+            deleted     = false;
             return *this;
         }
     };
 
     using Storage = std::vector<Item>;
 
-public:
-    HashTable(int reserve = 128):
-        _storage(Storage(reserve))
-    {}
-
-    float load_factor() const {
-        return float(used) / float(_storage.size());
+    // Makes sure we always use a power of 2 as size
+    static int round_size(int x) {
+        int size = 1;
+        while(size < x) {
+            size *= 2;
+        }
+        return size;
     }
 
-    bool get(const Key& name, Value& v) const {
-        Item const* item = _find(name);
+    public:
+    HashTable(int buckets = 128) : _storage(Storage(round_size(buckets))) {}
 
-        if (item == nullptr) {
+    float load_factor() const { return float(used) / float(_storage.size()); }
+
+    bool get(const Key &name, Value &v) const {
+        Item const *item = _find(name);
+
+        if(item == nullptr) {
             return false;
         }
 
@@ -110,18 +112,14 @@ public:
         return true;
     }
 
-    bool insert(const Key& name, const Value& value) {
-        return track_insert(name, value, false);
-    }
+    bool insert(const Key &name, const Value &value) { return track_insert(name, value, false); }
 
-    bool upsert(const Key& name, const Value& value) {
-        return track_insert(name, value, true);
-    }
+    bool upsert(const Key &name, const Value &value) { return track_insert(name, value, true); }
 
-    bool remove(const Key& name) {
-        Item* item = (Item*) _find(name);
+    bool remove(const Key &name) {
+        Item *item = (Item *)_find(name);
 
-        if (item == nullptr){
+        if(item == nullptr) {
             return false;
         }
 
@@ -130,21 +128,19 @@ public:
         return true;
     }
 
-    int size() const {
-        return used;
-    }
-    
+    int size() const { return used; }
+
     std::string __str__() const {
         std::stringstream ss;
-        bool comma = false;
-        bool all = false;
+        bool              comma = false;
+        bool              all   = false;
         ss << "{";
-        for (auto i = 0; i < _storage.size(); i++) {
+        for(auto i = 0; i < _storage.size(); i++) {
 
-            auto& item = _storage[i];
+            auto &item = _storage[i];
 
-            if (item.used && !item.deleted) {
-                if (comma) {
+            if(item.used && !item.deleted) {
+                if(comma) {
                     ss << ", ";
                 }
 
@@ -156,14 +152,58 @@ public:
         return ss.str();
     }
 
-    // Rebuild the hash using a bigger storage
-    void rehash(float multiplier = 2.f) {
-        Storage storage(int(_storage.size() * multiplier));
-        int a = 0;
-        int b = 0;
+    void resize(std::size_t n) { resize_force(n); }
 
-        for(auto& item: _storage) {
-            if (!item.used || item.deleted) {
+    // does not reinsert everything, but does not remove deleted entries
+    // this does not work yet
+    void resize_soft(std::size_t n) {
+        // n = 1024
+        // H::hash(key) == 1024 => (1024 % n) => 0
+        //
+        // resize to 2048
+        // H::hash(key) == 1024 => (1024 % n) = > 1024
+        //
+        //
+
+        n = round_size(n);
+        assert(is_power2(n));
+
+        uint64_t oldsize = _storage.size();
+        _storage.resize(round_size(n));
+
+        // TODO: remove deleted entries
+        for(auto &item : _storage) {
+            if(item.deleted || !item.used) {
+                continue;
+            }
+
+            uint64_t i = H::hash(item.key);
+
+            auto oldpos = P2_MOD(i, oldsize);
+            auto newpos = P2_MOD(i, n);
+
+            if(oldpos == newpos) {
+                continue;
+            }
+
+            for(uint64_t offset = 0; offset < _storage.size(); offset++) {
+                newpos = P2_MOD(i + offset, n);
+
+                if(!_storage[newpos].used) {
+                    std::swap(_storage[oldpos], _storage[newpos]);
+                }
+            }
+        }
+    }
+
+    // force a full resize with all the entries being reinserted
+    void resize_force(std::size_t n) {
+        Storage storage(round_size(n));
+        int     a = 0;
+        int     b = 0;
+
+        for(auto &item : _storage) {
+            if(!item.used || item.deleted) {
                 continue;
             }
             insert(storage, item, false, a, b);
@@ -172,81 +212,91 @@ public:
         _storage = storage;
     }
 
-private:
+    // Rebuild the hash using a bigger storage
+    // used multiplier == 1 to remove deleted entries
+    // can speed up insertion if a lot of keys got deleted
+    void rehash(float multiplier = 2.f) {
+        resize(_storage.size() * uint64_t(multiplier >= 1 ? multiplier : 2));
+    }
 
+    private:
     // this cannot be exposed because it returns a pointer
     // that could change once rehash is called
-    Item const* _find(const Key& name) const {
+    Item const *_find(const Key &name) const {
+        assert(is_power2(_storage.size()));
         uint64_t i = H::hash(name);
 
         // linear probing
         for(uint64_t offset = 0; offset < _storage.size(); offset++) {
-            //for(int m = -1; m < 2; m += 2) {
-                auto& item = _storage[REG_MOD((i + offset), _storage.size())];
+            // for(int m = -1; m < 2; m += 2) {
+            auto &item = _storage[P2_MOD((i + offset), _storage.size())];
 
-                // item was not used, so we know there was no collision
-                // and no linear probe insert was done after this
-                if (!item.used) {
-                    return nullptr;
-                }
+            // item was not used, so we know there was no collision
+            // and no linear probe insert was done after this
+            if(!item.used) {
+                return nullptr;
+            }
 
-                // Item is not deleted and key matches
-                if (!item.deleted && item.key == name) {
-                    return &item;
-                }
+            // Item is not deleted and key matches
+            if(!item.deleted && item.key == name) {
+                return &item;
+            }
             //}
         }
-    
+
         return nullptr;
     }
 
     // insert a key value pair
     // if the key already exist does not insert
-    bool track_insert(const Key& name, const Value& value, bool upsert) {
-        if (load_factor() >= 0.75) {
+    bool track_insert(const Key &name, const Value &value, bool upsert) {
+        if(load_factor() >= 0.75) {
             rehash();
         }
 
         auto item = Item(name, value);
         return insert(_storage, item, upsert, this->used, this->collision);
     }
-   
-    // insert is slower than std::unordered_map
-    static bool insert(Storage& data, Item const& inserted_item, bool upsert, int& used, int& collision) {
-        uint64_t i = H::hash(inserted_item.key);
-        
-        for(uint64_t offset = 0; offset < data.size(); offset++) {
-            //for(int m = -1; m < 2; m += 2) {
-                Item& item = data[REG_MOD((i + offset), data.size())];
 
-                if (!item.used || item.deleted) {
-                    item = inserted_item;
-                    used += 1;
+    // insert is slower than std::unordered_map
+    static bool insert(Storage &data, Item const &inserted_item, bool upsert, int &used,
+                       int &collision) {
+
+        assert(is_power2(data.size()));
+        uint64_t i = H::hash(inserted_item.key);
+
+        for(uint64_t offset = 0; offset < data.size(); offset++) {
+            // for(int m = -1; m < 2; m += 2) {
+            Item &item = data[P2_MOD((i + offset), data.size())];
+
+            if(!item.used || item.deleted) {
+                item = inserted_item;
+                used += 1;
+                return true;
+            }
+
+            if(item.key == inserted_item.key) {
+                if(upsert) {
+                    item.value = inserted_item.value;
                     return true;
                 }
+                return false;
+            }
 
-                if (item.key == inserted_item.key) {
-                    if (upsert) {
-                        item.value = inserted_item.value;
-                        return true;
-                    }
-                    return false;
-                }
-
-                collision += 1;
+            collision += 1;
             //}
         }
 
         return false;
     }
 
-    int used = 0;
-    int collision = 0;
+    int     used      = 0;
+    int     collision = 0;
     Storage _storage;
 
     public:
     int collided() const { return collision; }
 };
-}
+} // namespace easyfs
 
-#endif 
+#endif
--- tests/hashtable_test.h
+++ tests/hashtable_test.h
@@ -1,12 +1,12 @@
 #ifndef PROJECT_TEST_TESTS_ADD_HEADER
 #define PROJECT_TEST_TESTS_ADD_HEADER
 
-#include <gtest/gtest.h>
-#include <iostream>
 #include <chrono>
 #include <fstream>
-#include <unordered_map>
+#include <gtest/gtest.h>
+#include <iostream>
 #include <random>
+#include <unordered_map>
 
 #include "hashtable.h"
 
@@ -19,10 +19,17 @@ TEST(fast_mod, valid) {
     }
 }
 
+TEST(fast_mod_p2, valid) {
+    int n = 1024;
+    for(int i = 0; i < n * 2; i++) {
+        EXPECT_EQ(fast_mod_p2(i, n), i % n);
+    }
+}
+
 #define REPEAT 10000
 TEST(benchmark_mod, fast_mod) {
-    int n = 1093;
-    for(int j = 0; j < REPEAT; j++){
+    int n = 1024;
+    for(int j = 0; j < REPEAT; j++) {
         int volatile s = 0;
         for(int i = 0; i < n * 2; i++) {
             s += fast_mod(i, n);
@@ -31,8 +38,8 @@ TEST(benchmark_mod, fast_mod) {
 }
 
 TEST(benchmark_mod, reg_mod) {
-    int n = 1093;
-    for(int j = 0; j < REPEAT; j++){
+    int n = 1024;
+    for(int j = 0; j < REPEAT; j++) {
         int volatile s = 0;
         for(int i = 0; i < n * 2; i++) {
             s += reg_mod(i, n);
@@ -40,9 +47,18 @@ TEST(benchmark_mod, reg_mod) {
     }
 }
 
-TEST(hastable, base)
-{
-    int value = 0;
+TEST(benchmark_mod, fast_mod_p2) {
+    int n = 1024;
+    for(int j = 0; j < REPEAT; j++) {
+        int volatile s = 0;
+        for(int i = 0; i < n * 2; i++) {
+            s += fast_mod_p2(i, n);
+        }
+    }
+}
+
+TEST(hastable, base) {
+    int                         value = 0;
     HashTable<std::string, int> v;
 
     EXPECT_TRUE(v.insert("a", 2));
@@ -76,27 +92,14 @@ TEST(hastable, base)
 
     EXPECT_TRUE(v.remove("b"));
     EXPECT_EQ(v.size(), 1);
-
 }
 
-TEST(hastable, rehash)
-{
-    int value = 0;
+TEST(hastable, rehash) {
+    int                         value = 0;
     HashTable<std::string, int> v(6);
 
     std::vector<std::string> strings = {
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I",
-        "J",
-        "K",
-        "L",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
     };
 
     for(int i = 0; i < 12; i++) {
@@ -118,37 +121,21 @@ TEST(hastable, rehash)
     }
 }
 
-
 struct BadHash {
-    static uint64_t hash(std::string const&) noexcept {
-        return 0;
-    }
+    static uint64_t hash(std::string const &) noexcept { return 0; }
 };
 
-
-TEST(hastable, collision)
-{
+TEST(hastable, collision) {
     HashTable<std::string, int, BadHash> v(6);
 
     std::vector<std::string> strings = {
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I",
-        "J",
-        "K",
-        "L",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
     };
 
     for(int i = 0; i < 12; i++) {
         EXPECT_TRUE(v.insert(strings[i], i));
     }
-    
+
     EXPECT_EQ(v.size(), 12);
     // EXPECT_FLOAT_EQ(v.load_factor(), 1.f);
 
@@ -166,23 +153,11 @@ TEST(hastable, collision)
     }
 }
 
-TEST(hastable, collision_deletion)
-{
+TEST(hastable, collision_deletion) {
     HashTable<std::string, int, BadHash> v(6);
 
     std::vector<std::string> strings = {
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I",
-        "J",
-        "K",
-        "L",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
     };
 
     for(int i = 0; i < 6; i++) {
@@ -202,24 +177,25 @@ TEST(hastable, collision_deletion)
     EXPECT_LT(v.load_factor(), 1.f);
 }
 
-inline std::vector<std::tuple<std::string, int>> const& pairs() {
+#define SIZE 10000000
+inline std::vector<std::tuple<std::string, int>> const &pairs() {
     static std::vector<std::tuple<std::string, int>> data;
 
-    if (data.size() > 0) {
+    if(data.size() > 0) {
         return data;
     }
 
     auto f = std::ifstream("../tests/Honor of Thieves.txt");
 
-  std::default_random_engine generator;
-  std::uniform_int_distribution<int> distribution(0, 100000 * 10);
+    std::default_random_engine         generator;
+    std::uniform_int_distribution<int> distribution(0, SIZE);
 
-    while (f) {
+    while(f) {
         std::string s;
         f >> s;
-        data.push_back(std::make_tuple(s, distribution(generator)));
+        data.push_back(std::make_tuple(s, data.size()));
 
-        if (data.size() > 1000000) {
+        if(data.size() > SIZE) {
             break;
         }
     }
@@ -227,53 +203,46 @@ inline std::vector<std::tuple<std::string, int>> const& pairs() {
     return data;
 }
 
-TEST(benchmark, init) {
-    pairs();
-}
-
-
-TEST(benchmark_insert, init) {
-    pairs();
-}
-
-#define INIT 1093
-
-TEST(benchmark_insert_str, hashtable) {
-    HashTable<std::string, int> v(INIT);
-
-    for (auto& p: pairs()) {
-        v.insert(std::get<0>(p), std::get<1>(p));
+template <typename T> struct StdHash {
+    static uint64_t hash(T const &v) noexcept {
+        std::hash<T> h;
+        return h(v);
     }
+};
 
-    std::cout << v.collided() << std::endl;
-}
+TEST(benchmark, init) { pairs(); }
 
+TEST(benchmark_insert, init) { pairs(); }
+
+#define INIT 1024
 
 #define START() auto start = std::chrono::system_clock::now()
 #define DURATION() (std::chrono::duration<double>(end - start).count())
-#define END(name)\
-    auto end = std::chrono::system_clock::now();\
+#define END(name)                                                                                  \
+    auto end = std::chrono::system_clock::now();                                                   \
     std::cout << "[ TIME     ] " << name << " " << DURATION() * 1000.f << " ms\n";
 
-TEST(benchmark_lookup_str, hashtable) {
+TEST(benchmark_lookup_str, hashtable_siphash) {
     HashTable<std::string, int> v(INIT);
-    int value;
+    int                         value;
 
-    { 
+    {
         START();
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 100; i++) {
             v = HashTable<std::string, int>(INIT);
-            for (auto& p: pairs()) {
+            for(auto &p : pairs()) {
                 v.insert(std::get<0>(p), std::get<1>(p));
             }
         }
         END("insert");
     }
 
+    std::cout << "[ Collsion ] " << v.collided() << std::endl;
+
     {
         START();
-        for(int i = 0; i < 100; i++){
-            for (auto& p: pairs()) {
+        for(int i = 0; i < 100; i++) {
+            for(auto &p : pairs()) {
                 v.get(std::get<0>(p), value);
             }
         }
@@ -281,35 +250,55 @@ TEST(benchmark_lookup_str, hashtable) {
     }
 }
 
-TEST(benchmark_insert_int, hashtable) {
-    HashTable<int, std::string> v(INIT);
+TEST(benchmark_lookup_str, hashtable_stdhash) {
+    HashTable<std::string, int, StdHash<std::string>> v(INIT);
+    int                                               value;
 
-    for (auto& p: pairs()) {
-        v.insert(std::get<1>(p), std::get<0>(p));
+    {
+        START();
+        for(int i = 0; i < 100; i++) {
+            v = HashTable<std::string, int, StdHash<std::string>>(INIT);
+            for(auto &p : pairs()) {
+                v.insert(std::get<0>(p), std::get<1>(p));
+            }
+        }
+        END("insert");
     }
 
-    std::cout << v.collided() << std::endl;
+    std::cout << "[ Collsion ] " << v.collided() << std::endl;
+
+    {
+        START();
+        for(int i = 0; i < 100; i++) {
+            for(auto &p : pairs()) {
+                v.get(std::get<0>(p), value);
+            }
+        }
+        END("lookup");
+    }
 }
 
-TEST(benchmark_lookup_int, hashtable) {
+TEST(benchmark_lookup_int, hashtable_siphash) {
     HashTable<int, std::string> v(INIT);
-    std::string value;
+    std::string                 value;
 
-    { 
+    {
         START();
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 100; i++) {
             v = HashTable<int, std::string>(INIT);
-            for (auto& p: pairs()) {
+            for(auto &p : pairs()) {
                 v.insert(std::get<1>(p), std::get<0>(p));
             }
         }
         END("insert");
     }
 
+    std::cout << "[ Collsion ] " << v.collided() << std::endl;
+
     {
         START();
-        for(int i = 0; i < 100; i++){
-            for (auto& p: pairs()) {
+        for(int i = 0; i < 100; i++) {
+            for(auto &p : pairs()) {
                 v.get(std::get<1>(p), value);
             }
         }
@@ -317,31 +306,46 @@ TEST(benchmark_lookup_int, hashtable) {
     }
 }
 
+TEST(benchmark_lookup_int, hashtable_stdhash) {
+    HashTable<int, std::string, StdHash<int>> v(INIT);
+    std::string                               value;
 
-template<typename T>
-struct Siphash {
-    std::size_t operator()(const T& k) const
     {
-        return Hash<T>::hash(k);
+        START();
+        for(int i = 0; i < 100; i++) {
+            v = HashTable<int, std::string, StdHash<int>>(INIT);
+            for(auto &p : pairs()) {
+                v.insert(std::get<1>(p), std::get<0>(p));
+            }
+        }
+        END("insert");
     }
-};
 
-TEST(benchmark_insert_str, unordered_map) {
-    std::unordered_map<std::string, int, Siphash<std::string>> v(INIT);
+    std::cout << "[ Collsion ] " << v.collided() << std::endl;
 
-    for (auto& p: pairs()) {
-        v[std::get<0>(p)] = std::get<1>(p);
+    {
+        START();
+        for(int i = 0; i < 100; i++) {
+            for(auto &p : pairs()) {
+                v.get(std::get<1>(p), value);
+            }
+        }
+        END("lookup");
     }
 }
 
-TEST(benchmark_lookup_str, unordered_map) {
-    int value;
+template <typename T> struct Siphash {
+    std::size_t operator()(const T &k) const { return Hash<T>::hash(k); }
+};
+
+TEST(benchmark_lookup_str, unordered_map_siphash) {
+    int                                                        value;
     std::unordered_map<std::string, int, Siphash<std::string>> v(INIT);
     {
         START();
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 100; i++) {
             v = std::unordered_map<std::string, int, Siphash<std::string>>(INIT);
-            for (auto& p: pairs()) {
+            for(auto &p : pairs()) {
                 v[std::get<0>(p)] = std::get<1>(p);
             }
         }
@@ -350,8 +354,8 @@ TEST(benchmark_lookup_str, unordered_map) {
 
     {
         START();
-        for(int i = 0; i < 100; i++){
-            for (auto& p: pairs()) {
+        for(int i = 0; i < 100; i++) {
+            for(auto &p : pairs()) {
                 value = v[std::get<0>(p)];
             }
         }
@@ -359,23 +363,40 @@ TEST(benchmark_lookup_str, unordered_map) {
     }
 }
 
-TEST(benchmark_insert_int, unordered_map) {
-    std::unordered_map<int, std::string, Siphash<int>> v(INIT);
-
-    for (auto& p: pairs()) {
-        v[std::get<1>(p)] = std::get<0>(p);
+TEST(benchmark_lookup_str, unordered_map_stdhash) {
+    int                                  value;
+    std::unordered_map<std::string, int> v(INIT);
+    {
+        START();
+        for(int i = 0; i < 100; i++) {
+            v = std::unordered_map<std::string, int>(INIT);
+            for(auto &p : pairs()) {
+                v[std::get<0>(p)] = std::get<1>(p);
+            }
+        }
+        END("insert");
     }
-}
-
-TEST(benchmark_lookup_int, unordered_map) {
-    std::unordered_map<int, std::string, Siphash<int>> v(INIT);
-    std::string value;
 
     {
         START();
-        for(int i = 0; i < 100; i++){
-            v = std::unordered_map<int, std::string, Siphash<int>>(INIT);
-            for (auto& p: pairs()) {
+        for(int i = 0; i < 100; i++) {
+            for(auto &p : pairs()) {
+                value = v[std::get<0>(p)];
+            }
+        }
+        END("lookup");
+    }
+}
+
+TEST(benchmark_lookup_int, unordered_map_stdhash) {
+    std::unordered_map<int, std::string> v(INIT);
+    std::string                          value;
+
+    {
+        START();
+        for(int i = 0; i < 100; i++) {
+            v = std::unordered_map<int, std::string>(INIT);
+            for(auto &p : pairs()) {
                 v[std::get<1>(p)] = std::get<0>(p);
             }
         }
@@ -384,8 +405,8 @@ TEST(benchmark_lookup_int, unordered_map) {
 
     {
         START();
-        for(int i = 0; i < 100; i++){
-            for (auto& p: pairs()) {
+        for(int i = 0; i < 100; i++) {
+            for(auto &p : pairs()) {
                 value = v[std::get<1>(p)];
             }
         }
@@ -393,5 +414,30 @@ TEST(benchmark_lookup_int, unordered_map) {
     }
 }
 
+TEST(benchmark_lookup_int, unordered_map_siphash) {
+    std::unordered_map<int, std::string, Siphash<int>> v(INIT);
+    std::string                                        value;
+
+    {
+        START();
+        for(int i = 0; i < 100; i++) {
+            v = std::unordered_map<int, std::string, Siphash<int>>(INIT);
+            for(auto &p : pairs()) {
+                v[std::get<1>(p)] = std::get<0>(p);
+            }
+        }
+        END("insert");
+    }
+
+    {
+        START();
+        for(int i = 0; i < 100; i++) {
+            for(auto &p : pairs()) {
+                value = v[std::get<1>(p)];
+            }
+        }
+        END("lookup");
+    }
+}
 
 #endif
